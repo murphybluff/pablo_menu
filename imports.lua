d89733f1f927b1c8a0f8afc94792d122ef9ad7d6
jabby <61084256+murphybluff@users.noreply.github.com>
2025-02-17 12:27:24 +0800

    Update imports.lua

--- imports.lua
+++ imports.lua
@@ -36,7 +36,64 @@ function pablomenu(menuId, title, elements, callback)
 end
 
 ESX.OpenContext = function(menuId, elements, callback)
-    print("ESX.OpenContext called with menuId: " .. menuId) -- Debug log
     local title = elements[1] and elements[1].title or menuId
     pablomenu(menuId, title, elements, callback)
 end
+
+local function pablomenuHAHA(menuData)
+    local options = {}
+    
+    for _, item in ipairs(menuData) do
+        if not item.hidden then
+            local option = {
+                title = item.header,
+                description = item.txt or item.text,
+                disabled = item.disabled,
+                icon = item.icon,
+            }
+            
+    
+            if item.params then
+                if item.params.event then
+                    option.onSelect = function()
+                        if item.params.isServer then
+                            TriggerServerEvent(item.params.event, item.params.args)
+                        elseif item.params.isCommand then
+                            ExecuteCommand(item.params.event)
+                        elseif item.params.isAction then
+                            item.params.event(item.params.args)
+                        else
+                            TriggerEvent(item.params.event, item.params.args)
+                        end
+                    end
+                end
+            end
+            
+    
+            if not item.isMenuHeader then
+                table.insert(options, option)
+            end
+        end
+    end
+    
+   
+    lib.showContext({
+        id = 'converted_menu',
+        title = menuData[1]?.header or 'Menu', 
+        options = options
+    })
+end
+
+
+RegisterNetEvent('cfx-hu-menu:openMenu', function(data)
+    if GetResourceState('ox_lib') == 'started' then
+        exports['pablo_menu']:openMenu(data)
+    else
+        openMenu(data)
+    end
+end)
+
+-- Export function to use pablomenuHAHA
+exports('openMenu', function(menuData)
+    pablomenuHAHA(menuData)
+end)
